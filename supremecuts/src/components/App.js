6e05d8f63caa55c26abb470f5816fd01a7f8b891
ww2kvn <mwalthrust@gmail.com>
2019-09-05 15:48:47 -0400

    orginized files, created json data file, pulled json data using fetch promise

--- supremecuts/src/components/App.js
+++ supremecuts/src/components/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from '../images/logo.svg';
+import React, {Component} from 'react';
 import '../css/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component{
+  
+  constructor(){
+    super();
+    
+    this.state = {
+      barberApt : []
+    }
+  }
 
+  componentDidMount(){
+    
+    fetch('./barber_data.json')
+    .then(response => response.json())
+    .then(results =>{
+      const apts = results.map((items)=>{
+        return items;
+      });
+
+      this.setState({
+        baberApt : apts
+      });
+      
+    });
+
+  }
+  
+  render() {    
+    return (
+      <div className="header-title">
+        <h1> Supreme Cuts </h1>
+        <div>
+        
+        </div>
+      </div>
+    );
+  }
+}
 export default App;
